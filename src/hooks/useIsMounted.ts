b42a6f2dbb9e8bf22f0378850376ec8dc007002b
Heldzs <51962581+Heldzs@users.noreply.github.com>
2026-07-15 10:37:19 -0400

    feat: add custom hook useIsMounted for component mount status

---
+++ src/hooks/useIsMounted.ts
@@ -0,0 +1,13 @@
+import { useSyncExternalStore } from "react";
+
+const emptySubscribe = () => () => {};
+
+export function useIsMounted() {
+  const isMounted = useSyncExternalStore(
+    emptySubscribe,
+    () => true,
+    () => false,
+  );
+
+  return isMounted;
+}
